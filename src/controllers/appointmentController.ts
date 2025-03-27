439993276ce168cee7fcdc8d52cf8b72927d62f7
Samuel Andrade de Araújo <andradedearaujo100@gmail.com>
2025-03-27 09:33:00 -0300

    feat: controladores implementados

---
+++ src/controllers/appointmentController.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express'
+import { appointmentService } from '../services/appointmentService'
+import { AppointmentStatus } from '@prisma/client'
+
+export const appointmentController = {
+    async createAppointment(req: Request, res: Response) {
+        try {
+            const appointment = await appointmentService.createAppointment(req.body)
+            return res.status(201).json(appointment);
+        } catch (error) {
+            return res.status(400).json({ error: (error as Error).message})
+        }
+    },
+
+    async listAppointments(req: Request, res: Response) {
+        try {
+            const { userId } = req.query;
+            const appointments = await appointmentService.listAppointment(userId as string);
+            return res.status(200).json(appointments);
+        } catch (error) {
+            return res.status(400).json({ error: (error as Error).message })
+        }
+    },
+
+    async updateAppointmentStatus(req: Request, res: Response) {
+        try {
+            const { appointmentId } = req.params
+            const { status } = req.body
+
+            if (!Object.values(AppointmentStatus).includes(status)) {
+                return res.status(400).json({ error: 'Status inválido!' })
+            }
+
+            const appointment = await appointmentService.updateAppointmentStatus(appointmentId, status)
+            return res.status(200).json(appointment)            
+        } catch (error) {
+            return res.status(400).json({ error: (error as Error).message })
+        }
+    }
+}
---
+++ src/controllers/userController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express'
+import { userService } from '../services/userService'
+
+export const UserController = {
+    async createUser(req: Request, res: Response) {
+        try {
+            const user = await userService.createUser(req.body)
+            return res.status(201).json(user)
+        } catch (error) {
+            return res.status(400).json({ error: (error as Error).message })
+        }
+    },
+
+    async authenticateUser(req: Request, res: Response) {
+        try {
+            const { email, password } = req.body
+            const result = await userService.authenticateUser(email, password)
+            return res.status(200).json(result);
+        } catch (error) {
+            return res.status(401).json({ error: (error as Error).message });
+        }
+    }
+}
